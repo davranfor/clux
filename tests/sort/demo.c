cb5b1ea9274963692f1b4e009e5eb1d7c23e6d18
David RF <davranfor@gmail.com>
2024-01-05 08:55:21 +0100

    Add user-defined callback example in the sort test

--- tests/sort/demo.c
+++ tests/sort/demo.c
@@ -8,24 +8,43 @@
 #include <time.h>
 #include <clux/json.h>
 
+static int nulls_first(const json *a, const json *b)
+{
+    return json_is_null(b) - json_is_null(a); 
+}
+
 int main(void)
 {
     srand((unsigned)time(NULL));
 
     json *root = json_new_array(NULL);
 
-    for (int i = 0; i < 25; i++)
+    for (int i = 0; i <= 25; i++)
     {
         json_push_back(root, json_new_integer(NULL, rand() % 100));
+        if ((i % 5) == 0)
+        {
+            json_push_back(root, json_new_null(NULL));
+        }
     }
+
     puts("Unsorted:");
     json_print(root);
-    puts("Sorted:");
+
+    // Sort by predefined callback
+    puts("Sorted by value (ascendent):");
     json_sort(root, JSON_SORT_BY_VALUE_ASC);
     json_print(root);
+
+    // Sort by user-defined callback
+    puts("Sorted by nulls first:");
+    json_sort(root, nulls_first);
+    json_print(root);
+
     puts("Reversed:");
     json_reverse(root);
     json_print(root);
+
     json_free(root);
     return 0;
 }
