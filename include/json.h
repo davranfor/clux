b97a0aa31e5af59a829aabbeb9ef809b5edb07a2
David RF <davranfor@gmail.com>
2024-06-07 14:05:31 +0200

    Removed json_map

--- include/json.h
+++ include/json.h
@@ -28,10 +28,8 @@ enum json_encode
 };
 
 typedef struct json json;
-typedef struct json_map json_map;
 typedef struct {int line, column;} json_error;
 typedef int (*json_walk_callback)(const json *, int, void *);
-typedef int (*json_map_walk_callback)(json *, size_t, void *);
 typedef int (*json_sort_callback)(const json *, const json *);
 
 // ============================================================================
@@ -128,16 +126,6 @@ int json_write_file(const json *, const char *, int);
 int json_print(const json *);
 char *json_path(const json *);
 char *json_quote(const char *);
-// ============================================================================
-// Map
-// ============================================================================
-json_map *json_map_create(const char *, size_t);
-json *json_map_insert(json_map *, json *);
-json *json_map_delete(json_map *, const char *);
-json *json_map_search(const json_map *, const char *);
-json *json_map_walk(const json_map *, json_map_walk_callback, void *);
-size_t json_map_size(const json_map *);
-void json_map_destroy(json_map *, void (*)(json *));
 // ============================================================================
 // Pointer
 // ============================================================================
