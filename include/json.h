0b059bc82f7a3498718ad5ee00230173778c4c68
David RF <davranfor@gmail.com>
2024-10-26 10:40:13 +0200

    Cleaning code

--- include/json.h
+++ include/json.h
@@ -11,10 +11,10 @@
 #include "json_reader.h"
 #include "json_writer.h"
 #include "json_buffer.h"
-#include "json_schema.h"
-#include "json_sort.h"
 #include "json_pointer.h"
+#include "json_schema.h"
 #include "json_patch.h"
+#include "json_sort.h"
 #include "json_map.h"
 
 #endif
--- tests/parse_file/demo.c
+++ tests/parse_file/demo.c
@@ -24,10 +24,10 @@ static void parse_file(const char *path)
     }
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
     setlocale(LC_CTYPE, "");
-    parse_file("test.json");
+    parse_file(argc > 1 ? argv[1] : "test.json");
     return 0;
 }
 
