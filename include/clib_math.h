23348210e2250c8662d44c1638f38bd1b622ba06
David RF <davranfor@gmail.com>
2024-11-09 11:24:18 +0100

    Check if a number is in range [LONG_LONG_MIN ... LONG_LONG_MAX] in IS_SAFE_INTEGER if USE_SAFE_INTEGER is not defined

--- include/clib_math.h
+++ include/clib_math.h
@@ -9,15 +9,17 @@
 
 #include <stddef.h>
 
-/**
- * Check whether 'number' can be exactly represented as an IEEE-754
- * double precision number without rounding.
- */
 #if defined(USE_SAFE_INTEGER)
+/**
+ * Check whether 'number' can be exactly represented as an
+ * IEEE-754 double precision number without rounding.
+ */
 #define IS_SAFE_INTEGER(number) \
     (!(((number) < -9007199254740991.0) || ((number) > 9007199254740991.0)))
 #else
-#define IS_SAFE_INTEGER(number) 1
+/* Check whether 'number' is in range [LONG_LONG_MIN ... LONG_LONG_MAX] */
+#define IS_SAFE_INTEGER(number) \
+    (!(((number) < -9223372036854775807.0) || ((number) > 9223372036854775807)))
 #endif
 
 size_t next_pow2(size_t);
