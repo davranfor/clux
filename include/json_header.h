85d1e8bad0cb8294c9e49f82f03648d0cf930d9d
David RF <davranfor@gmail.com>
2025-01-29 16:55:10 +0100

    Add JSON_PRAGMA directive

--- include/json_header.h
+++ include/json_header.h
@@ -14,12 +14,18 @@
 
 #define JSON_NOT_FOUND -1u
 
+#if defined(__clang__)
+#define JSON_PRAGMA(x) x
+#else
+#define JSON_PRAGMA(x) #x
+#endif
+
 /* Cast 'const json_t *' to 'json_t *' without warning */
 #define json_cast(node)                                                     \
-    _Pragma("GCC diagnostic push")                                          \
-    _Pragma("GCC diagnostic ignored \"-Wcast-qual\"")                       \
+    _Pragma(JSON_PRAGMA("GCC diagnostic push"))                             \
+    _Pragma(JSON_PRAGMA("GCC diagnostic ignored \"-Wcast-qual\""))          \
     _Generic((node), const json_t *: ((json_t *)(node)), default: (node))   \
-    _Pragma("GCC diagnostic pop")
+    _Pragma(JSON_PRAGMA("GCC diagnostic pop"))
 
 enum json_type
 {
