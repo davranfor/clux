8ac5ec62eae4ece131255fe0a0b812a856d930e2
David RF <davranfor@gmail.com>
2025-02-24 22:42:21 +0100

    Cleaning code

--- include/json_private.h
+++ include/json_private.h
@@ -14,7 +14,7 @@ struct json
     unsigned size;          // Size of an iterable (object/array)
     unsigned short flags;   // Available for user
     unsigned char packed;   // 0 = Root node | 1 = Packed node
-    unsigned char type;     // json_type_t compressed (1 byte)
+    unsigned char type;     // json_type compressed (1 byte)
 };
 
 #endif
