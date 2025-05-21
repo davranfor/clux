3ede7afbc2acc85415569254157bb6ec8563722c
David RF <davranfor@gmail.com>
2025-05-21 21:25:27 +0200

    Improving punch stuff in tests/http

---
+++ tests/http/static/functions.js
@@ -0,0 +1,17 @@
+function shortDateTime(datetime) {
+  const [date, time] = datetime.split(' ');
+  const [y, m, d] = date.split('-');
+  const [h, M, s] = time.split(':');
+  
+  return `${d}/${m}/${y} ${h}:${M}`;
+}
+
+function longDateTime(datetime) {
+  const [date, time] = datetime.split(' ');
+  const [y, m, d] = date.split('-');
+  
+  return `${d}/${m}/${y} ${time}`;
+}
+
+
+
