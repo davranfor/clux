9215be101c632caf1823439a5a0106ce80b9f4b6
David RF <davranfor@gmail.com>
2025-05-25 20:25:44 +0200

    clocking improved in tests/http

--- tests/http/static/timesheet.js
+++ tests/http/static/timesheet.js
@@ -1,35 +1,24 @@
 const clocking = {
-    time: null, epoch: null, timeout: 0, running: false,
+    time: null, timeout: 0, running: false,
     text: document.getElementById('clocking-text'),
     update() {
-        this.text.textContent = formatTime(Date.now() - this.epoch);
+        this.text.textContent = formatTime(Date.now() - this.time);
         this.timeout = setTimeout(() => this.update(), 1000 - (Date.now() % 1000));
     },
-    start(time = null) {
-        if (this.timeout) clearTimeout(this.timeout);
-        if (time) {
-            const dateParts = time.split(/[- :]/);
-            const dateObj = new Date(
-                parseInt(dateParts[0]),     // year
-                parseInt(dateParts[1]) - 1, // month (0-11)
-                parseInt(dateParts[2]),     // day
-                parseInt(dateParts[3]),     // hours
-                parseInt(dateParts[4]),     // minutes
-                parseInt(dateParts[5])      // seconds
-            );
-
-            this.epoch = dateObj.getTime();
-        } else {
-            this.epoch = Date.now();
+    start(time) {
+        if (!this.running) {
+            if (this.timeout) clearTimeout(this.timeout);
+            this.text.textContent = '00:00:00';
+            this.time = time;
+            this.running = true;
+            this.update();
         }
-        this.text.textContent = '00:00:00';
-        this.running = true;
-        this.update();
     },
     stop() {
         if (this.running) {
             clearTimeout(this.timeout);
             this.text.textContent = '';
+            this.time = Date.now();
             this.running = false;
         }
     }
@@ -68,7 +57,7 @@ async function logHours() {
             user.workplace = data[0];
             user.clockInTime = data[2] === 0 ? data[1] : 0;
         } else if (response.status === 204) {
-            throw new Error('Deben transcurrir al menos 5 segundos entre fichajes');
+            throw new Error('No se puede fichar en este momento');
         } else {
             const text = await response.text();
             throw new Error(text || `HTTP Error ${response.status}`);
