a6b1e94cd1ecaeb094af38721c7ecae18f852bd2
David RF <davranfor@gmail.com>
2025-05-26 02:31:08 +0200

    Adding week table in tests/http

--- tests/http/static/functions.js
+++ tests/http/static/functions.js
@@ -1,10 +1,27 @@
 function formatTime(ms) {
     const totalSeconds = Math.floor(ms / 1000);
+
     return [
         Math.floor(totalSeconds / 3600).toString().padStart(2, '0'),
         Math.floor((totalSeconds % 3600) / 60).toString().padStart(2, '0'),
         (totalSeconds % 60).toString().padStart(2, '0')
     ].join(':');
+}
+
+function timeDiff(a, b) {
+    const diffMs = a - b;
+    const diffMin = Math.abs(Math.floor(diffMs / 60000)); // 1 min = 60000 ms
+    const h = Math.floor(diffMin / 60);
+    const m = diffMin % 60;
+    const sign = diffMs < 0 ? '-' : '';
+
+    return `${sign}${String(h)}h${String(m).padStart(2, '0')}m`;
+}
+
+function weekDay(datetime) {
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+    return days[datetime.getDay()];
 }
 
 function shortDateTime(datetime) {
