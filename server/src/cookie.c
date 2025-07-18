533ec4ca771c785aa78ae467152205855964a4af
David RF <davranfor@gmail.com>
2025-07-18 02:15:53 +0200

    Continue with html stuff in tests/http

--- server/src/cookie.c
+++ server/src/cookie.c
@@ -11,53 +11,56 @@
 #include <openssl/rand.h>
 #include "cookie.h"
 
-#define TOKEN_SIZE 65
+#define TOKEN_SIZE 64
 
+/**
+ * Parse cookie in the form:
+ * Cookie: [third-party-cookie;] auth=<int>:<int>:<hex 64 bytes> [third-party-cookie]
+ * If 'auth' is not found and '/login' is not found, then return 0 (Unauthorized)
+ */
 int cookie_parse(cookie_t *cookie, const char *path, char *str)
 {
-    if ((str = strstr(str, "Cookie: auth=")))
+    if ((str = strstr(str, "\r\nCookie: ")))
     {
-        str += 13;
+        str += 10;
 
         char *end = strchr(str, '\r');
 
-        if (end == NULL)
-        {
-            return -1;
-        }
         *end = '\0';
-
-        int data[2] = {0};
-
-        for (int i = 0; i < 2; i++)
+        if ((str = strstr(str, "auth=")) && ((str[-1] == ' ') || (str[-1] == ';')))
         {
-            char *ptr;
+            str += 5;
 
-            data[i] = (int)strtol(str, &ptr, 10);
-            if ((*ptr != ':') || (data[i] <= 0))
+            int data[2] = {0};
+
+            for (int i = 0; i < 2; i++)
+            {
+                char *ptr;
+
+                data[i] = (int)strtol(str, &ptr, 10);
+                if ((*ptr != ':') || (data[i] <= 0))
+                {
+                    return -1;
+                }
+                str = ptr + 1;
+            }
+            if (end - str < TOKEN_SIZE)
             {
                 return -1;
             }
-            str = ptr + 1;
+            str[TOKEN_SIZE] = '\0';
+            cookie->user = data[0];
+            cookie->role = data[1];
+            cookie->token = str;
+            return 1;
         }
-        if (end - str != TOKEN_SIZE - 1)
-        {
-            return -1;
-        }
-        cookie->user = data[0];
-        cookie->role = data[1];
-        cookie->token = str;
-        return 1;
     }
-    else if (!strcmp(path, "/login"))
+    if (!strcmp(path, "/login"))
     {
         cookie->token = "";
         return 1;
     }
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
 
 int cookie_create(int user, int role, const char *token, char *cookie)
@@ -68,7 +71,7 @@ int cookie_create(int user, int role, const char *token, char *cookie)
     }
     else
     {
-        unsigned char bytes[TOKEN_SIZE / 2];
+        unsigned char bytes[(TOKEN_SIZE + 1) / 2];
 
         if (RAND_bytes(bytes, sizeof(bytes)) != 1)
         {
@@ -81,7 +84,7 @@ int cookie_create(int user, int role, const char *token, char *cookie)
         {
             snprintf(output + (i * 2), 3, "%02x", bytes[i]);
         }
-        output[TOKEN_SIZE - 1] = '\0';
+        output[TOKEN_SIZE] = '\0';
     }
     return 1;
 }
