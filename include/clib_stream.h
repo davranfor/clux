d81c13a06d40dd7f84da4c1d4e8b4410a6ad218f
David RF <davranfor@gmail.com>
2025-12-21 23:30:36 +0100

    file_read_callback() receives a pointer to non const

--- include/clib_stream.h
+++ include/clib_stream.h
@@ -9,7 +9,7 @@
 
 int file_exists(const char *);
 char *file_read(const char *);
-char *file_read_callback(const char *, char *(*)(const void *, size_t), const void *);
+char *file_read_callback(const char *, char *(*)(void *, size_t), void *);
 int file_write(const char *, const char *);
 int file_write_bytes(const char *, const char *, size_t);
 int file_delete(const char *);
