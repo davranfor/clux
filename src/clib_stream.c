7124739cbb8bd5f6d4e0813eacb7e37253c47ffc
David RF <davranfor@gmail.com>
2025-12-25 12:52:45 +0100

    Change headers order in clib_stream.c

--- src/clib_stream.c
+++ src/clib_stream.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <sys/stat.h>
 #include "clib_stream.h"
 
